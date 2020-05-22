ebd86aa9eb1606979154ec3413fcce73098e4e51
Simon <simonfong6@gmail.com>
2020-05-21 17:26:16 -0700

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import SushiGoRound from './game';
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const canvas = document.getElementById('game-canvas');
-  const game = new SushiGoRound(canvas);
-  console.log('Webpack is working!');
-});
